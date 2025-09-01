5093408a9978edcc6968f4b7624b8693c78446bf
VonziePemula <vonzie79@gmail.com>
2025-09-01 11:40:59 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+export default {
+  owner: "62857734669112", // nomor admin
+  ai_api: "https://api-inference.huggingface.co/models/gpt2", 
+  ai_token: "AIzaSyDFIM1osR9WwWtu4lGbc9LZNM2_2hjxHH8" // atau OpenAI API Key
+}
